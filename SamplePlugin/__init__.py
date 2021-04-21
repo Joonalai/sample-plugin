f0fab74e36a91acd33e1f3c9b467860c6be1720e
Joonalai <joona@gispo.fi>
2021-04-21 14:27:39 +0300

    Add noqa messages

--- SamplePlugin/__init__.py
+++ SamplePlugin/__init__.py
@@ -2,10 +2,11 @@
 
 
 # if os.environ.get('QGIS_PLUGIN_USE_DEBUGGER') == 'pydevd':
-#     if os.environ.get('IN_TESTS', "0") != "1" and os.environ.get('QGIS_PLUGIN_IN_CI', "0") != "1":
+#     if os.environ.get('IN_TESTS', "0") != "1" and os.environ.get('QGIS_PLUGIN_IN_CI', "0") != "1": # noqa
 #         setup_pydevd()
 
 
-def classFactory(iface):
+def classFactory(iface):  # noqa: N802
     from .plugin import Plugin
+
     return Plugin(iface)
